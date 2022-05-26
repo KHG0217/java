a4bb8ccec3326d3c4a1b0e4e5f47b441901adef1
kim <hyukgyu3818@naver.com>
2022-05-26 18:30:05 +0900

    220526 -학원 문제푸는중

--- Game/src/test/main/Main.java
+++ Game/src/test/main/Main.java
@@ -8,37 +8,29 @@ import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Integer> set1=new ArrayList<>();
-		int budget=10;
-		int result=0;
-		int [] d=new int[4];
-		d[0]=2;
-		d[1]=2;
-		d[2]=3;
-		d[3]=3; 
+		String[] id_list = new String[4];
+		id_list[0]="muzi";
+		id_list[1]="frodo";
+		id_list[2]="apeach";
+		id_list[3]="neo";	
+		String[] report = new String[5];
 		
+		report[0]=id_list[0]+" "+id_list[1];
+		report[1]=id_list[2]+" "+id_list[1];
+		report[2]=id_list[1]+" "+id_list[3];
+		report[3]=id_list[0]+" "+id_list[3];
+		report[4]=id_list[2]+" "+id_list[0];
 		
-		for(int i=0; i<d.length; i++) {
-			set1.add(d[i]);
-		}
-			 
-		Collections.sort(set1);
-		Iterator<Integer> it=set1.iterator();
-		int a=it.next();
-			while(budget-a>=0) {				
-				if(budget-a==0) {
-					result++;
-					break;
-				}
-				
-				budget=budget-a;			
-				result++;
-				a=it.next();
-											
-			}
-			
+		if(int i =0; i<report.)
 	
-			System.out.println(result);	
-
-    }
+		
+		// answer 신고한 id 누적이 2개이상인 id 하나당 1 {2, 1, 1, 0}?
+		//  ->  신고당한 2번이상 k++ 2?
+		
+		
+		int k;
+		int[] answer = {};
+	
+	}
+	
 }
