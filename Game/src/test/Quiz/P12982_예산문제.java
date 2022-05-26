11e0d20284513fd43caa100d961ec4731cfcaf0e
kimhyukgyu <hyukgyu3818@naver.com>
2022-05-26 22:13:50 +0900

    220526 집-1

--- Game/src/test/Quiz/P12982_예산문제.java
+++ Game/src/test/Quiz/P12982_예산문제.java
@@ -1,29 +1,37 @@
-package test.Quiz;
-import java.util.ArrayList;
+package P12982;
+
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-public class P12982_예산문제 {
 
-	    public int solution(int[] d, int budget) {
-	        int answer = 0;
-	        List<Integer> set1=new ArrayList<>();
-	        for(int i=0; i<d.length; i++) {
-				set1.add(d[i]);
-			}       
-	        Collections.sort(set1);
-			Iterator<Integer> it=set1.iterator();
-			int a=it.next();
-				while(budget-a>=0) {				
-					if(budget-a==0) {
+class KHG0217 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        List<Integer> set1=new ArrayList<>();
+        for(int i=0; i<d.length; i++) {
+			set1.add(d[i]);
+		}
+        
+        Collections.sort(set1);
+		
+		int a = set1.get(0);
+		
+		while(budget-set1.get(0)>=0) {	
+			if(budget-set1.get(0)==0) {
+				answer++;
+				break;
+			}else {
+				for(int i=0; i<set1.size(); i++) {
+					if(budget-set1.get(i)>=0) {
+						budget=budget-set1.get(i);
 						answer++;
-						break;
-					}				
-					budget=budget-a;			
-					answer++;
-					a=it.next();
+					}
 				}
-	        return answer;
-	    }
-	
+				break;
+			}
+		}
+		return answer;
+	}
+			   
 }
